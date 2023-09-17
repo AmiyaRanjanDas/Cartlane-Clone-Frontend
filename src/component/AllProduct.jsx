105f9c21b39016cb5d24473cd690b13bd713ae22
Amiya Ranjan Das <amiyaranjandas@gmail.com>
2023-09-17 11:50:42 +0530

    commit

--- src/component/AllProduct.jsx
+++ src/component/AllProduct.jsx
@@ -117,32 +117,32 @@ const AllProduct = () => {
           </button>
           <ul className="dropdown-menu btn-info">
             <li>
-              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?cat=bracelet">
+              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?catagory=bracelet">
                 BRACELET
               </a>
             </li>
             <li>
-              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?cat=necklace">
+              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?catagory=necklace">
                 NECKLACE
               </a>
             </li>
             <li>
-              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?cat=pendant">
+              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?catagory=pendant">
                 PENDENTS
               </a>
             </li>
             <li>
-              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?cat=earrings">
+              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?catagory=earrings">
                 RINGS
               </a>
             </li>
             <li>
-              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?cat=bangles">
+              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?catagory=bangles">
                 BANGLES
               </a>
             </li>
             <li>
-              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?cat=earrings">
+              <a className="dropdown-item" href="/Cartlane-Clone-Frontend/allproduct?catagory=earrings">
                 EAR-RINGS
               </a>
             </li>
